9c69515eed09968e4acd56aeddde7645dd60b703
DavidRiccio <dricciodeabreu1@gmail.com>
2025-11-25 18:40:58 +0000

    Se crea el primer servicio rest

---
+++ servicios/spring-rest/src/main/java/com/docencia/rest/model/User.java
@@ -0,0 +1,70 @@
+package com.docencia.rest.model;
+
+import java.util.Objects;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    private int id;
+    private String name;
+
+    /**
+     * Constuctor vacio
+     */
+    public User() {
+
+    }
+
+    /**
+     * Constructor de User con parametro nombre
+     * 
+     * @param name Nombre del usuario
+     */
+    public User(String name) {
+        this.name = name;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Column(name = "name", nullable = false)
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        User other = (User) obj;
+        return id == other.id;
+    }
+
+    @Override
+    public String toString() {
+        return "User [id=" + id + ", name=" + name + "]";
+    }
+}

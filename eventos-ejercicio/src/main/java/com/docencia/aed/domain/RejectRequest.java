d4c7309cf9e503bf8da2a091ccd0ebf80be1a8f9
DavidRiccio <dricciodeabreu1@gmail.com>
2026-01-22 16:56:24 +0000

    Se empieza la tarea evaluable 3.3

---
+++ eventos-ejercicio/src/main/java/com/docencia/aed/domain/RejectRequest.java
@@ -0,0 +1,16 @@
+package com.docencia.aed.domain;
+
+import jakarta.validation.constraints.NotBlank;
+
+public class RejectRequest {
+    @NotBlank
+    private String reason;
+
+    public String getReason() {
+        return reason;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+}
